f22cf7a8463f9fb50dd9ba3ee7ebd0eee545922c
luke.macausland <luke.macausland@torqit.ca>
2025-03-18 10:45:30 -0300

    Setting up bundle.

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TorqIT\SelectOptionsCreatorBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('select_options_creator');
+
+        return $treeBuilder;
+    }
+}
---
+++ src/SelectOptionsCreatorBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TorqIT\SelectOptionsCreatorBundle;
+
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
+use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
+
+class SelectOptionsCreatorBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
+{
+    use BundleAdminClassicTrait;
+}
